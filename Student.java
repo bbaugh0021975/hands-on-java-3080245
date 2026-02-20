858c14e44fd92fcd664d93a87a186399cc8af554
bbaugh0021975 <brian_baugh@comcast.com>
2026-02-20 19:58:25 +0000

    Module 1 Project 1: Student Class

---
+++ Student.java
@@ -0,0 +1,39 @@
+public class Student {
+
+    private String name;
+    private int age;
+    private String grade;
+
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+
+    
+
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGrade() {
+    return this.grade;
+  }
+
+  public void setGrade(String grade) {
+    this.grade = grade;
+  }
+
+}
